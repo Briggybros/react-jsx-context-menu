68b1378fba56d5a797074f9e4de91bbb91bd7617
Gregory Sims <briggybros.2@googlemail.com>
2018-01-01 23:59:56 +0000

    Bugfixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,41 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, 'lib', 'index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'react-context-menu.min.js',
-    library: 'react-context-menu',
-  },
-  module: {
-    loaders: [{
-      test: /\.j(s|sx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['env', 'stage-0', 'react'],
-      },
-    }],
-  },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
+    entry: path.join(__dirname, 'lib', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'react-context-menu.min.js',
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
+    module: {
+        loaders: [{
+            test: /\.j(s|sx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['env', 'stage-0', 'react'],
+            },
+        }],
     },
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: true,
-    }),
-  ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+        },
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: true,
+        }),
+    ],
 };
